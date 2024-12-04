24af17d733eb7e4645c3108aa44db1f00c89720c
Igor Gissi <gissiigor@gmail.com>
2024-12-04 13:30:29 -0300

    Criação dos personagens exemplo

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ class personagem{
     }
     atacar(){
         let ataque = "espada"
-        console.log(`o tipo ${tipo} atacou usando ${ataque}`)
+        console.log(`o tipo ${this.tipo} atacou usando ${ataque}`)
     }
 }
-personagem.atacar()
+let personagemExemploGuerreiro = new personagem("Kyupes", "22", "guerreiro")
+let personagemExemploNinja = new personagem ("Bluhite", "34", "Ninja")
+let personagemExemploMago = new personagem("Jokeymin", "143", "Mago")
+let personagemExemploMonge = new personagem("Plygius", "84", "Monge")
+
+personagemExemploNinja.atacar()
