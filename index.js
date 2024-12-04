68455cc0c630b4bfbbb53dccd28dd3028f6624c8
Igor Gissi <gissiigor@gmail.com>
2024-12-04 13:37:38 -0300

    Adicionado ações para teste do código

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class personagem{
                 arma = "Artes Marciais"
                 break
         }
-        console.log(`o tipo ${this.tipo} atacou usando ${ataque}`)
+        console.log(`o tipo ${this.tipo} atacou usando ${arma}`)
     }
 }
 let personagemExemploGuerreiro = new personagem("Kyupes", "22", "Guerreiro")
@@ -28,4 +28,7 @@ let personagemExemploNinja = new personagem("Bluhite", "34", "Ninja")
 let personagemExemploMago = new personagem("Jokeymin", "143", "Mago")
 let personagemExemploMonge = new personagem("Plygius", "84", "Monge")
 
+personagemExemploGuerreiro.atacar()
 personagemExemploNinja.atacar()
+personagemExemploMago.atacar()
+personagemExemploMonge.atacar()
